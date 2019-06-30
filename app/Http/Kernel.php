867aac267431c66c7a2c1556577c3d6d396c51db
Biya Kazmi <kazmib@mcmaster.ca>
2019-06-30 14:34:38 -0400

    Added multi authorization for Students and Instructors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,10 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'student' => \App\Http\Middleware\RedirectIfNotStudent::class,
+        'student.guest' => \App\Http\Middleware\RedirectIfStudent::class,
+        'instructor' => \App\Http\Middleware\RedirectIfNotInstructor::class,
+        'instructor.guest' => \App\Http\Middleware\RedirectIfInstructor::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,30 @@ Route::post('{quiz}/question', 'TFQuestionController@store');
 Route::get('/testbank', 'TestBankController@index')->name('TestBank');
 
 Route::get('midterm1', 'Midterm1Controller@index')->name('M1');
+Route::group(['prefix' => 'instructor'], function () {
+  Route::get('/login', 'InstructorAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'InstructorAuth\LoginController@login');
+  Route::post('/logout', 'InstructorAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'InstructorAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'InstructorAuth\RegisterController@register');
+
+  Route::post('/password/email', 'InstructorAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'InstructorAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'InstructorAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'InstructorAuth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'student'], function () {
+  Route::get('/login', 'StudentAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'StudentAuth\LoginController@login');
+  Route::post('/logout', 'StudentAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'StudentAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'StudentAuth\RegisterController@register');
+
+  Route::post('/password/email', 'StudentAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'StudentAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'StudentAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'StudentAuth\ResetPasswordController@showResetForm');
+});
