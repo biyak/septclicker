47558720bbf87be96688a3bc18ee5c0f19e024b4
Biya Kazmi <kazmib@mcmaster.ca>
2019-07-18 11:15:35 -0400

    bad changes

--- routes/web.php
+++ routes/web.php
@@ -34,20 +34,7 @@ Route::get('/quizinteractive', 'QuizInteractiveController@index')->name('QuizInt
 Route::get('/instructor/home', 'InstructorHomeController@index')->name('InstructorHome');
 
 
-Route::group(['prefix' => 'instructor'], function () {
-  Route::get('/login', 'InstructorAuth\LoginController@showLoginForm')->name('login');
-  Route::post('/login', 'InstructorAuth\LoginController@login');
-  Route::post('/logout', 'InstructorAuth\LoginController@logout')->name('logout');
-
-  Route::get('/register', 'InstructorAuth\RegisterController@showRegistrationForm')->name('register');
-  Route::post('/register', 'InstructorAuth\RegisterController@register');
-
-  Route::post('/password/email', 'InstructorAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
-  Route::post('/password/reset', 'InstructorAuth\ResetPasswordController@reset')->name('password.email');
-  Route::get('/password/reset', 'InstructorAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
-  Route::get('/password/reset/{token}', 'InstructorAuth\ResetPasswordController@showResetForm');
-
-  Route::get('quizlist/{user}', 'InstructorQuizListController@index')->name('instructorquizlist.show');
+Route::get('quizlist/{user}', 'InstructorQuizListController@index')->name('instructorquizlist.show');
 
 
 //Creating and displaying questions
@@ -86,18 +73,3 @@ Route::post('/active', 'QuizController@store');
   Route::get('/testbank', 'TestBankController@index')->name('TestBank');
 
   Route::get('midterm1', 'Midterm1Controller@index')->name('M1');
-});
-
-Route::group(['prefix' => 'student'], function () {
-  Route::get('/login', 'StudentAuth\LoginController@showLoginForm')->name('login');
-  Route::post('/login', 'StudentAuth\LoginController@login');
-  Route::post('/logout', 'StudentAuth\LoginController@logout')->name('logout');
-
-  Route::get('/register', 'StudentAuth\RegisterController@showRegistrationForm')->name('register');
-  Route::post('/register', 'StudentAuth\RegisterController@register');
-
-  Route::post('/password/email', 'StudentAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
-  Route::post('/password/reset', 'StudentAuth\ResetPasswordController@reset')->name('password.email');
-  Route::get('/password/reset', 'StudentAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
-  Route::get('/password/reset/{token}', 'StudentAuth\ResetPasswordController@showResetForm');
-});
