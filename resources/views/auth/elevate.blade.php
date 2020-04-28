a4512ec3b16518d90863b1d2966629fe428893c8
Biya Kazmi <kazmib@mcmaster.ca>
2020-04-28 15:35:57 -0400

    Updated Elevate to include course name and course code

--- resources/views/auth/elevate.blade.php
+++ resources/views/auth/elevate.blade.php
@@ -23,6 +23,22 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="coursecode" class="col-md-4 col-form-label text-md-right">{{ __('Course Code: ') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="coursecode" type="text" class="form-control" name="coursecode" value="" required autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="coursename" class="col-md-4 col-form-label text-md-right">{{ __('Course Name: ') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="coursename" type="text" class="form-control" name="coursename" value="" required autofocus>
+                            </div>
+                        </div>
+
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
